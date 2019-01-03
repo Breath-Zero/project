1a3ae0282950de25bf421a924931fd3e0bbd582b
Breath-Zero <1395043294@qq.com>
2019-01-03 16:25:27 +0800

    <接口与异常的练习-更新> 2019/1/3 <叶>

--- LX/src/Test.java
+++ LX/src/Test.java
@@ -43,6 +43,43 @@
  * 4.创建一个带默认构造方法（即无参构造）的类，在构造方法中打印一条消息"Hello Constructor";
  * 再为这个类添加一个重载构造方法，令其接收一个字符串参数，并在这个有参构造方法中把
  * "Hello Constructor"和接收的参数一起打印出来。
+ * <p>
+ * 1.创建一个Test类，包含有一个public权限的int型成员变量与一个char类型的成员变量，观察在main方法中的初始值。
+ * <p>
+ * 2.编写一个程序，展示无论你创建了某个特定类的多少个对象，这个类的某个特定的static成员变量只有一个属性。
+ * <p>
+ * 3.一个斐波那契数列是由数字1、1、2、3、5、8、13、21、34等等组成的，其中每一个数字(从第三个数字起)
+ * 都是前两个数字的和。创建一个方法，接受一个整数参数，并显示从第一个元素开始总共由该参数指定的个
+ * 数所构成的所有斐波那契数字。例如，如果运行 java Fibonacci 5(Fibonacci为类名)，那么输出应该
+ * 是1、1、2、3、5。
+ * <p>
+ * 4.创建一个带默认构造方法（即无参构造）的类，在构造方法中打印一条消息"Hello Constructor";
+ * 再为这个类添加一个重载构造方法，令其接收一个字符串参数，并在这个有参构造方法中把
+ * "Hello Constructor"和接收的参数一起打印出来。
+ * <p>
+ * 1.创建一个Test类，包含有一个public权限的int型成员变量与一个char类型的成员变量，观察在main方法中的初始值。
+ * <p>
+ * 2.编写一个程序，展示无论你创建了某个特定类的多少个对象，这个类的某个特定的static成员变量只有一个属性。
+ * <p>
+ * 3.一个斐波那契数列是由数字1、1、2、3、5、8、13、21、34等等组成的，其中每一个数字(从第三个数字起)
+ * 都是前两个数字的和。创建一个方法，接受一个整数参数，并显示从第一个元素开始总共由该参数指定的个
+ * 数所构成的所有斐波那契数字。例如，如果运行 java Fibonacci 5(Fibonacci为类名)，那么输出应该
+ * 是1、1、2、3、5。
+ * <p>
+ * 4.创建一个带默认构造方法（即无参构造）的类，在构造方法中打印一条消息"Hello Constructor";
+ * 再为这个类添加一个重载构造方法，令其接收一个字符串参数，并在这个有参构造方法中把
+ * "Hello Constructor"和接收的参数一起打印出来。
+ * <p>
+ * static关键字的作用：
+ * （1）设置变量的存储域，函数体内static变量的作用范围为该函数体，不同于auto变量，
+ * 该变量的内存只被分配一次，因此其值在下次调用时仍维持上次的值；
+ * （2）限制变量的作用域，在模块内的static全局变量可以被模块内所用函数访问，
+ * 但不能被模块外其它函数访问；
+ * （3）限制函数的作用域，在模块内的static函数只可被这一模块内的其它函数调用，
+ * 这个函数的使用范围被限制在声明它的模块内；
+ * （4）在类中的static成员变量意味着它为该类的所有实例所共享，也就是说当某个类的实例修改了该静态成员变量，
+ * 其修改值为该类的其它所有实例所见；
+ * （5）在类中的static成员函数属于整个类所拥有，这个函数不接收this指针，因而只能访问类的static成员变量。
  */
 
 //public class Test {
@@ -423,4 +460,406 @@
 //    }
 //}
 
+//// ========================================
 
+/**
+ * 2.编程题:
+ * 利用接口做参数，写个计算器，能完成加减乘除运算。
+ * （1）定义一个接口Compute含有一个方法int computer(int n, int m)。
+ * （2）设计四个类分别实现此接口，完成加减乘除运算。
+ * （3）设计一个类UseCompute，类中含有方法：public void useCom(Compute com, int one, int two)，此方法能够用传递过来的对象调用computer方法完成运算，并输出运算的结果。
+ * （4）设计一个主类Test，调用UseCompute中的方法useCom来完成加减乘除运算。
+ * <p>
+ * 3.按如下要求编写Java程序：
+ * （1）定义接口A，里面包含值为3.14的常量PI和抽象方法double area()。
+ * （2）定义接口B，里面包含抽象方法void setColor(String c)。
+ * （3）定义接口C，该接口继承了接口A和B，里面包含抽象方法void volume()。
+ * （4）定义圆柱体类Cylinder实现接口C，该类中包含三个成员变量：底圆半径radius、
+ * 圆柱体的高height、颜色color。
+ * （5）创建主类来测试类Cylinder。
+ * <p>
+ * 4.(附加题-算法)
+ * 一个数如果恰好等于它的因子之和，这个数就称为 "完数 "。例如6=1＋2＋3.编程 找出1000以内的所有完数。
+ */
+
+//interface InterfaceA {
+//    String S = "good ";
+//
+//    void f();
+//}
+//
+//abstract class ClassA {
+//    abstract void g();
+//}
+//
+//class ClassB extends ClassA implements InterfaceA {
+//    void g() {
+//        System.out.print(S);
+//    }
+//
+//    public void f() {
+//        System.out.print(" " + S);
+//    }
+//}
+//
+//public class Test {
+//    public static void main(String[] args) {
+//        ClassA a = new ClassB();
+//        InterfaceA b = new ClassB();
+//        a.g();
+//        b.f();
+//    }
+//}
+
+// 2、计算器
+// -------------------------------------
+
+//interface Computer {
+//    int computer(int n, int m);
+//}
+//
+//class Add implements Computer {
+//
+//    @Override
+//    public int computer(int n, int m) {
+//        System.out.print(n + " + " + m + " = ");
+//        return n + m;
+//    }
+//}
+//
+//class Minus implements Computer {
+//
+//    @Override
+//    public int computer(int n, int m) {
+//        System.out.print(n + " - " + m + " = ");
+//        return n - m;
+//    }
+//}
+//
+//class Multiply implements Computer {
+//
+//    @Override
+//    public int computer(int n, int m) {
+//        System.out.print(n + " x " + m + " = ");
+//        return n * m;
+//    }
+//}
+//
+//class Divide implements Computer {
+//
+//    @Override
+//    public int computer(int n, int m) {
+//        if (m != 0) {
+//            System.out.print(n + " / " + m + " = ");
+//            return n / m;
+//        }else {
+//            System.out.println("输入错误，除数不能为0！！！");
+//            return 0;
+//        }
+//    }
+//}
+//
+//class UseComputer {
+//    public void usecomputer(Computer com, int n, int m) {
+//        System.out.println(com.computer(n, m));
+//    }
+//}
+//
+//public class Test{
+//    public static void main(String[] args) {
+//        UseComputer useCom = new UseComputer();
+//        useCom.usecomputer(new Add(), 12, 6);
+//        useCom.usecomputer(new Minus(), 12, 6);
+//        useCom.usecomputer(new Multiply(), 12, 6);
+//        useCom.usecomputer(new Divide(), 12, 6);
+//        useCom.usecomputer(new Divide(), 12, 0);
+//    }
+//}
+
+// 3、
+//------------------------------------------
+
+//interface A {
+//    double PI = 3.14;
+//
+//    double area(); // 底面积
+//}
+//
+//interface B {
+//    void setColor(String c);
+//}
+//
+//interface C extends A, B {
+//    void volume(); // 体积
+//}
+//
+//class Cylinder implements C {
+//    private double radius;
+//    private double heigth;
+//    private String color;
+//
+//    public Cylinder(double radius, double heigth, String color) {
+//        this.radius = radius;
+//        this.heigth = heigth;
+//        this.color = color;
+//    }
+//
+//    @Override
+//    public double area() {
+//        System.out.println("圆柱体底面积为：" + PI * radius * radius);
+//        return PI * radius * radius;
+//    }
+//
+//    @Override
+//    public void volume() {
+//        System.out.println("圆柱体积为：" + area() * heigth);
+//    }
+//
+//    @Override
+//    public void setColor(String color) {
+//        System.out.println("圆柱体颜色为：" + color);
+//    }
+//}
+//
+//public class Test {
+//    public static void main(String[] args) {
+//        Cylinder cylinder = new Cylinder(1.2, 3.2, "red");
+//        cylinder.volume();
+//        cylinder.setColor("red");
+//    }
+//}
+
+// 4、--------------------------------------------
+
+//public class Test{
+//    public static void main(String[] args) {
+//        System.out.println("1000以内的完数有:");
+//        print_wanshu();
+//    }
+//
+//    public static void print_wanshu(){
+//        for (int i = 1; i <= 1000; i++) {
+//            int sum = 0;
+//            for (int j = 1; j <= i / 2; j++) {
+//                if (i % j == 0) {
+//                    sum += j;
+//                }
+//            }
+//            if (sum == i) {
+//                System.out.print(i + "、");
+//            }
+//        }
+//    }
+//}
+
+//// =================================================================
+
+import java.util.Objects;
+import java.util.Scanner;
+
+/**
+ * 2.编程题:
+ * 要求:
+ * 1).Person类有name,age,salary属性，要求实现至少两个构造方法，并且属性私有，提供对应的getter、setter。
+ * 2).覆写toString方法，要求在System.out.println()函数中传递Person对象能打印出三个属性值而不是对象地址。
+ * 3).覆写equals方法，要求两个Person类对象的值相同时返回true。
+ *
+ * 3.说出下列程序的执行结果，并说明原因:
+ * Integer a = 55;
+ * Integer b = 55;
+ * System.out.println(a==b);
+ * System.out.println(a==new Integer(55));
+ * System.out.println(a.equals(new Integer(55)));
+ * Integer c = 129;
+ * Integer d = 129;
+ * System.out.println(c==d);
+ *
+ * 4.写出懒汉式单例模式
+ *
+ * 5.编程:
+ * 1).定义一个MulException类继承Exception类，要求两数相乘等于100报错，
+ *    在主类中定义一个方法，在方法中抛出此异常，在主方法观察结果。
+ * 2).定义一个DivException类继承RuntimeException类，要求两数相除等于2报错，
+ *   在主类中定义一个方法，在方法中抛出此异常，在主方法观察结果。
+ *
+ * 6.(算法)
+ * 题目：猴子吃桃问题：猴子第一天摘下若干个桃子，当即吃了一半，还不过瘾，
+ * 又多吃了一个 第二天早上又将剩下的桃子吃掉一半，又多吃了一个。以后每天
+ * 早上都吃了前一天剩下的一半零一个。到第10天早上想再吃时，见只剩下一个桃子了。
+ * 求第一天共摘了多少。
+ */
+
+//
+//class Person{
+//    private String name;
+//    private int age;
+//    private int salary;
+//
+//    public Person(){
+//        System.out.println("Person的无参构造");
+//    }
+//
+//    public Person(String name, int age, int salary) {
+//        super();
+//        this.name = name;
+//        this.age = age;
+//        this.salary = salary;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//
+//    public int getSalary() {
+//        return salary;
+//    }
+//
+//    public void setSalary(int salary) {
+//        this.salary = salary;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "姓名：" + name + "年龄：" + age + "工资：" + salary;
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o)
+//            return true;
+//        if (!(o instanceof Person))
+//            return false;
+//        Person person = (Person) o;
+//        return age == person.age &&
+//                salary == person.salary &&
+//                Objects.equals(name, person.name);
+//    }
+//
+//}
+//
+//public class Test{
+//    public static void main(String[] args) {
+//        Person person = new Person("张三",18,2000);
+//        Person person1 = new Person("张三",18,2000);
+//        System.out.println(person.equals(person1));
+//        System.out.println(person.toString());
+//    }
+//}
+
+//// ---------------------------------------
+
+//public class Test{
+//    public static void main(String[] args) {
+//        Integer a = 55;
+//        Integer b = 55;
+//        System.out.println(a==b);
+//        System.out.println(a==new Integer(55));
+//        System.out.println(a.equals(new Integer(55)));
+//        Integer c = 129;
+//        Integer d = 129;
+//        System.out.println(c==d);
+//    }
+//}
+
+//// 懒汉式单例-------------------------------------------
+
+//class Singleton{
+//    private static Singleton instance;
+//    private Singleton(){}
+//
+//    public static Singleton getInstance() {
+//        if (instance == null) {
+//            instance = new Singleton();
+//        }
+//        return instance;
+//    }
+//
+//    public void print() {
+//        System.out.println("Hello World!!!");
+//    }
+//}
+//public class Test{
+//    public static void main(String[] args) {
+//        Singleton singleton = null;
+//        singleton = Singleton.getInstance();
+//        singleton.print();
+//    }
+//}
+
+/// --------------------------------------------
+
+//class MulException extends Exception{
+//    public MulException(String msg){
+//        super(msg);
+//    }
+//}
+//
+//class DivException extends RuntimeException{
+//    public DivException(String msg){
+//        super(msg);
+//    }
+//}
+//
+//public class Test{
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("请输入两个整数：");
+//        Integer num1 = scanner.nextInt();
+//        Integer num2 = scanner.nextInt();
+//        System.out.println(Mul(num1,num2));
+////        System.out.println(Div(num3,num4));
+//    }
+//    public static int Mul(int x, int y){
+//        int result = x*y;
+//        if(result == 100){
+//            try {
+//                throw new MulException("两数相乘等于100！！");
+//            } catch (MulException e) {
+//                e.printStackTrace();
+//            }
+//            return 0;
+//        }
+//        else
+//            return result;
+//    }
+//    public static int Div(int x, int y){
+//        int result = x/y;
+//        if(result == 2){
+//            try {
+//                throw new DivException("两数相除等于2！！");
+//            } catch (DivException e) {
+//                e.printStackTrace();
+//            }
+//            return 0;
+//        }
+//        else
+//            return result;
+//    }
+//}
+
+//// ----------------------------------------------
+
+//public class Test{
+//    public static void main(String[] args) {
+//        int num = print_num(1);
+//        System.out.println(num);
+//    }
+//    public static int print_num(int num){
+//        if(10 == num){
+//            return 1;
+//        }
+//        return (print_num(num+1)+1)*2;
+//    }
+//}
